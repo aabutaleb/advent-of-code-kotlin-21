4ffdce21ae5880cbbdc4c5f840e9b91310e16ad4
Amin Abu-Taleb <abta.amin@gmail.com>
2021-12-07 22:59:30 +0000

    A bit of a dirty hack to navigate down from the median ,could be done more elegantly as the number could also be higher than the median but not too far

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,23 +1,46 @@
+import kotlin.math.abs
+import kotlin.math.roundToInt
+
 fun main() {
-    fun calculateCosts(input: List<Int>, median: Int): Int {
+    fun calculateCostsLinear(input: List<Int>, median: Int): Int {
         var cost = 0
-        input.forEach { cost += Math.abs(it - median) }
+        input.forEach { cost += abs(it - median) }
+        return cost
+    }
+
+    fun calculateCostsIncremental(input: List<Int>, mean: Int): Int {
+        var cost = 0
+        input.forEach {
+            var distance = abs(it - mean)
+            cost += ((1+distance)*distance)/2
+        }
         return cost
     }
 
     fun part1(input: List<Int>): Int {
         var median = input.sorted().get(input.size/2)
-        return calculateCosts(input, median)
+        return calculateCostsLinear(input, median)
     }
 
-    //Three-measurement sliding window
     fun part2(input: List<Int>): Int {
-        return 0
+        var mean = input.average().roundToInt()
+        var cost = calculateCostsIncremental(input, mean)
+        println("Mean $mean Cost $cost")
+        var iter = mean - 1
+        var newCost = calculateCostsIncremental(input, iter)
+        println("Pivot $iter Cost $newCost")
+        while (newCost < cost){
+            cost = newCost
+            iter--
+            newCost = calculateCostsIncremental(input, iter)
+            println("Pivot $iter Cost $newCost")
+        }
+        return Math.min(cost, newCost)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readCslInt("Day07_test")
-    check(part1(testInput) == 37)
+    check(part2(testInput) == 168)
 
     val input = readCslInt("Day07")
     println(part1(input))
