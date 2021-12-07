8aa13a38eedad6875314063a712b0c6eff15205d
Amin Abu-Taleb <abta.amin@gmail.com>
2021-12-07 21:00:30 +0000

    Day2!!

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,11 +1,13 @@
 fun main() {
+
     fun part1(input: List<String>): Int {
-        var pos =  IntArray(2) {0}
         // first in pair is the array index in the position, second is the
         // positive/negative multiplier of depth
         val instructions = mapOf<String, Pair<Int, Int>>("forward" to Pair(0, 1),
-                                                         "down" to Pair (1, 1),
-                                                         "up" to Pair (1, -1))
+                "down" to Pair (1, 1),
+                "up" to Pair (1, -1))
+
+        var pos =  IntArray(2) {0}
 
         input.map { it.split(" ") }.forEach {
             var i: Pair<Int, Int>? = instructions[it[0]]
@@ -16,12 +18,30 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        // first in Triple is the array index in the position, second is the
+        // positive/negative multiplier of depth, third is the aim multiplier, works for up/down, special case
+        // on forward
+        val instructions = mapOf<String, Pair<Int, Int>>("forward" to Pair(0, 1),
+                "down" to Pair (2, 1),
+                "up" to Pair (2, -1))
+
+        var pos =  IntArray(3) {0}
+
+        input.map { it.split(" ") }.forEach {
+            var i: Pair<Int, Int>? = instructions[it[0]]
+            if (i != null) {
+                pos[i.first] += it[1].toInt() * i.second
+                if (it[0].equals("forward")) // Special case, could be improved
+                    pos[1] += pos[2] * it[1].toInt()
+            }
+
+        }
+        return pos[0] * pos[1]
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInputString("Day02_test")
-    check(part1(testInput) == 150)
+    check(part2(testInput) == 900)
 
     val input = readInputString("Day02")
     println(part1(input))
